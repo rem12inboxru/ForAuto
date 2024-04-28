8211e1f601fead84e3788dda291110bb127f504d
rem12inboxru <166938917+rem12inboxru@users.noreply.github.com>
2024-04-28 16:11:08 +0800

    Add files via upload

---
+++ ForAuto.py
@@ -0,0 +1,7 @@
+listAuto = ['BMW', 'MB', 'Lada', 'KIA', 'Honda']
+for i in listAuto:
+    print("Я езжу на автомобиле марки ", i)
+cars_count = 0
+for i in range(2, 10):
+    cars_count = cars_count + 10
+    print(cars_count)
